7db881096558212bdb6c7f26165646437b482055
Eduardo <jocdwardo25@gmail.com>
2023-07-06 04:09:33 -0500

    proyect coupons backend

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  typeDocument: {
+    type: String,
+    required: true,
+  },
+  numberDocument: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  store: {
+    type: String,
+    required: true,
+  },
+  cuopon: {
+    type: String,
+    required: false,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
