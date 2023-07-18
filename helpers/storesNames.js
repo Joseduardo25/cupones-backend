d5dba912dcdecc99a48a941b7ca6e3c458a3369e
Eduardo <jocdwardo25@gmail.com>
2023-07-18 11:10:20 -0500

    update storeName.js

--- helpers/storesNames.js
+++ helpers/storesNames.js
@@ -1,7 +1,5 @@
 export const storeName = {
-  // pucallpa1_tarapaca: "francisco.trujillo@credivargas.pe",
-  //EMAIL TEST
-  pucallpa1_tarapaca: "jose.allpas@gmail.com",
+  pucallpa1_tarapaca: "francisco.trujillo@credivargas.pe",
   iquitos: "catherine.chacaltana@credivargas.pe",
   jaen: "abmer.gaya@credivargas.pe",
   tarapoto: "paola.pons@credivargas.pe",
